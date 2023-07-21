ed20b1ba8caf37eec09467221e9ceb8375b658ad
Mitchell Hanberg <mitch@mitchellhanberg.com>
2023-07-21 13:27:42 -0400

    chore: bump 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Lazyasdf.MixProject do
   def project do
     [
       app: :lazyasdf,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       releases: releases(),
